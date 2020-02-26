a254b17a6e5f90ec0cad9a5073b259e2aff50a05
LORYMAX4 <lorymax4@gmail.com>
2020-02-26 09:51:42 +0100

    Aggiunti metodi DELETE e GET/id

--- src/resttest/RestTest.java
+++ src/resttest/RestTest.java
@@ -18,50 +18,73 @@ public class RestTest
             URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             Scanner keyboard = new Scanner(System.in);
+            OutputStream os;
+            BufferedReader br;
             int risposta;
             String in;
             System.out.println("Quale operazione vuoi eseguire: \n"+
-                    "1) Output di tutti gli impiegati.\n"+
-                    "2) Inserimento di un impiegato.\n"+
-                    "3) Eliminazione di un impiegato tramite id. \n"+
-                    "4) Output di un impiegato tramite id. \n"+
-                    "5) Modificare un solo attributo di un impiegato. \n"+
-                    "6) Modificare di un nuovo impiegato.\n");
+                    "1) Output di tutti gli impiegati(GET).\n"+
+                    "2) Inserimento di un impiegato/(POST).\n"+
+                    "3) Eliminazione di un impiegato tramite id(DELETE). \n"+
+                    "4) Output di un impiegato tramite id(GET/id). \n"+
+                    "5) Modificare un solo attributo di un impiegato(PATCH). \n"+
+                    "6) Modificare di un nuovo impiegato(PUT).\n");
             risposta = keyboard.nextInt();
             switch(risposta)
             {
                 case 1:
                     conn.setRequestMethod("GET");
                     conn.setRequestProperty("Accept", "application/json");
+                    br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                    String output;
+                    System.out.println("Output from Server .... \n");
+                    while ((output = br.readLine()) != null)
+                    {
+                            System.out.println(output);
+                    }
+                    conn.disconnect();
                     break;
                 case 2:
                     conn.setRequestMethod("POST");
                     conn.setRequestProperty("Content-Type", "application/json");
                     conn.setDoOutput(true);
+                    os= conn.getOutputStream();
                     in = "{\"employeeId\":4,\"firstName\":\"Lorenzo\",\"lastName\":\"Merlini\",\"email\":\"lorenzomerlini@gmail.com\",\"phone\":\"3524759669\"}";
-                    OutputStream os = conn.getOutputStream();
                     os.write(in.getBytes());
                     os.flush();
-                    System.out.println("Inserimento avvenuto correttamente");
+                    System.out.println("Inserimento avvenuto correttamente.");
                     break;
                 case 3:
+                    url = new URL("http://localhost:8080/api/tutorial/1.0/employees/4");
+                    conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("DELETE");
                     conn.setRequestProperty("Accept", "application/json");
-                    
+                    System.out.println("Cancellazione avvenuta correttamente.");
+                    break;
+                case 4:
+                    url = new URL("http://localhost:8080/api/tutorial/1.0/employees/4");
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("Accept", "application/json");
+                    br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                    System.out.println("Output from Server .... \n");
+                    while ((output = br.readLine()) != null)
+                    {
+                            System.out.println(output);
+                    }
+                    conn.disconnect();
+                    break;
+                case 5:
+                    url = new URL("http://localhost:8080/api/tutorial/1.0/employees/4");
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("PATCH");
+                    conn.setRequestProperty("Accept", "application/json");                    
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Opzione non supportata.");
             }
-            if (conn.getResponseCode() != 200)
-            {
-                    throw new RuntimeException("Failed : HTTP error code : "
-                                    + conn.getResponseCode());
-            }
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null)
-            {
-                    System.out.println(output);
-            }
-            conn.disconnect();
       } 
       catch (MalformedURLException e) 
       {
